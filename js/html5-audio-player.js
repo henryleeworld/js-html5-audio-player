e9efc5514f701cd1b1c0f7196ad97e1cac555f63
Henry Lee <henryleeworld@gmail.com>
2020-01-22 11:19:35 +0800

    feat:初始化

---
+++ js/html5-audio-player.js
@@ -0,0 +1,18 @@
+Amplitude.init({
+  "songs": [
+    {
+      "name": "Fantastic",
+      "artist": "鈴木あみ",
+      "album": "Fantastic",
+      "url": "music/fantastic.mp3",
+      "cover_art_url": "images/fantastic-album-cover.jpg"
+    },
+    {
+      "name": "JUST DREAM ON",
+      "artist": "諸岡ケンジ&JACK BITES",
+      "album": "機甲警察メタルジャック JUST DREAM ON",
+      "url": "music/just-dream-on.mp3",
+      "cover_art_url": "images/just-dream-on-album-cover.jpg"
+    }
+  ]
+});
